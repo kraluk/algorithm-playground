5116d5c76c7c982e175553ad4c81c8e7c4bbacc8
Lukasz Krauzowicz <kraluk@gmail.com>
2017-11-25 23:50:14 +0100

    Fixed issue with Blackhole instance after upgrading JMH to version 1.19.

--- src/jmh/java/com/kraluk/playground/algorithm/euler/EvenFibonacciNumbersBenchmark.java
+++ src/jmh/java/com/kraluk/playground/algorithm/euler/EvenFibonacciNumbersBenchmark.java
@@ -1,34 +1,27 @@
 package com.kraluk.playground.algorithm.euler;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
 @BenchmarkMode(Mode.AverageTime)
 @State(Scope.Benchmark)
 public class EvenFibonacciNumbersBenchmark {
 
-    private Blackhole blackhole = new Blackhole();
-
     @Param({"400000", "4000000"})
     public long maximum;
 
     @Benchmark
-    public void calculateBenchmark() {
+    public void calculateBenchmark(Blackhole blackhole) {
         blackhole.consume(EvenFibonacciNumbers.calculate(maximum));
     }
 
     @Benchmark
-    public void calculateWhiledBenchmark() {
+    public void calculateWhiledBenchmark(Blackhole blackhole) {
         blackhole.consume(EvenFibonacciNumbers.calculateWhiled(maximum));
     }
 
     @Benchmark
-    public void calculateOptimizedBenchmark() {
+    public void calculateOptimizedBenchmark(Blackhole blackhole) {
         blackhole.consume(EvenFibonacciNumbers.calculateOptimized(maximum));
     }
 }
